5e61588b94b0dfcb200ad783d7d84415c3f60456
Avelino <t@avelino.xxx>
2020-08-03 07:36:32 -0300

    warning: package comment should be of the form: Package nan

--- sql.go
+++ sql.go
@@ -1,6 +1,6 @@
+package nan
 //From https://github.com/golang/go/blob/go1.14.6/src/database/sql/convert.go
 //sql.Null* types use these methods internally but they're not exposed.
-package nan
 
 import (
 	"database/sql"
